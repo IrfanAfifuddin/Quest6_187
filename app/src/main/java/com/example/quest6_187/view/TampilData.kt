ace882b8820e53a77f67eeb547f547e4acfade24
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-21 12:55:42 +0700

    membuat value item nama

--- app/src/main/java/com/example/quest6_187/view/TampilData.kt
+++ app/src/main/java/com/example/quest6_187/view/TampilData.kt
@@ -31,9 +31,7 @@ fun TampilData(
     onBackBtnClick: ()->Unit
 ){
     val items = listOf(
-        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
-        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
-        Pair(stringResource(id = R.string.alamat),"Yogyakarta")
+        Pair(stringResource(id = R.string.nama_lengkap), statusUiSiswa.nama),
     )
     Scaffold (modifier = Modifier,
         {
